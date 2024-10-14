dbf2c7a28af6746522bc962e740709ef44f3ab69
salambayo3 <103267763+salambayo3@users.noreply.github.com>
2024-10-14 18:28:57 +0100

    Add files via upload

---
+++ question.js
@@ -0,0 +1,134 @@
+// Creating an array and passing the number,question and answers
+
+let questions = [
+    {
+        number: 1,
+        question: "What is the capital of Nigeria?",
+        answer: "Abuja",
+        options: [
+            "Lagos",
+            "Port-Harcourt",
+            "Calabar",
+            "Abuja",
+        ]
+    },
+
+    {
+        number: 2,
+        question: "What does HTML stands for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Markup Language",
+            "Hyper Text Makeup Languege",
+            "Hyper Test Markup Language",
+            "Hypper Text markup Language",
+        ]
+    },
+
+    {
+        number: 3,
+        question: "What does CSS stands for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Cascading Style Sheet",
+            "Cacading Style Sheet",
+            "Casceding Styling Sheet",
+            "Cascading Style Shete",
+        ]
+    },
+
+    {
+        number: 4,
+        question: "What does SQL stands for?",
+        answer: "Structured Query Language",
+        options: [
+            "Structured Quary Language",
+            "Structured Query Linguage",
+            "Structure Query Language",
+            "Structured Query Language",
+        ]
+    },
+
+    {
+        number: 5,
+        question: "A line that divide a circle into two equal parts is called?",
+        answer: "diameter",
+        options: [
+            "sector",
+            "chord",
+            "diameter",
+            "radius",
+        ]
+    },
+    // {
+    //     number: 6,
+    //     question: "A sachet of pure water cost #1.00,if 20 sachets bought at once would be given for #150,how much is saved on hundred sachet bought?",
+    //     answer: "#250",
+    //     options: [
+    //         "#150",
+    //         "#250",
+    //         "#200",
+    //         "#350",
+    //     ]
+    // },
+    // {
+    //     number: 7,
+    //     question: "How many geopolitical zones is nigeria divided into?",
+    //     answer: "Six",
+    //     options: [
+    //         "Eight",
+    //         "Five",
+    //         "Four",
+    //         "Six",
+    //     ]
+    // },
+    // {
+    //     number: 8,
+    //     question: "A nucleus has a mass number of 12 and an atomic number of 6.What is the number of neutrons in the nucleus?",
+    //     answer: "Six",
+    //     options: [
+    //         "Eight",
+    //         "Five",
+    //         "Four",
+    //         "Six",
+    //     ]
+    // },
+    // {
+    //     number: 9,
+    //     question: "What is S.I unit of inductance?",
+    //     answer: "Henry",
+    //     options: [
+    //         "Farads",
+    //         "Henry",
+    //         "Joule",
+    //         "Newton",
+    //     ]
+    // },
+
+    // {
+    //     number: 6,
+    //     question: "What is the process called when plants make their own food?",
+    //     answer: "Photosynthesis",
+    //     options: [
+    //         "Digestion",
+    //         "Photosynthesis",
+    //         "Excretion",
+    //         "Reproduction",
+    //     ]
+    // },
+    // {
+    //     number: 7,
+    //     question: "What is the capital of Nigeria?",
+    //     answer: "Abuja",
+    //     options: [
+    //         "Lagos",
+    //         "Port-Harcourt",
+    //         "Calabar",
+    //         "Abuja",
+    //     ]
+    // },
+
+
+
+
+]
